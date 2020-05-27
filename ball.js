f904023bc814acc941d69244fb5e1c7ecf257a37
asahyo <56744354+asahyo@users.noreply.github.com>
2020-05-27 13:34:36 -1000

    Add files via upload

--- ball.js
+++ ball.js
@@ -1,42 +1,44 @@
-function Ball() {
-    this.pos = createVector(width / 2, height / 2);
+function Ball(){
+    this.pos = createVector(width/2, height/2);
     this.r = 30;
-    this.direction = createVector(1, 1); //movement of the ball 
-    this.vel = createVector(1, 1).mult(5);
+    this.direction = createVector(1, 1);
+    //the number will be how fast your ball moves
+    this.vel = createVector(1, 1).mult(6);
 
-    this.display = function() {
-        ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+    this.display = function(){
+        ellipse(this.pos.x, this.pos.y, this.r *2, this.r*2)
     }
 
-    this.update = function() {
-        this.pos.x += this.vel.x * this.direction.x;
-        this.pos.y += this.vel.x * this.direction.y;
+    this.update = function(){
+        this.pos.x +=this.vel.x * this.direction.x;
+        this.pos.y +=this.vel.y * this.direction.y;
     }
 
     this.checkEdges = function() {
-        if (this.pos.y < this.r && this.direction.y < 0) {
-            this.direction.y *= -1; //by multiplying the y, it reverses the direction 
-        }
-        /*else if (this.pos.y > height - this.r && this.direction.y > 0) {
+        //if we know the ball is hitting the top, we reverse the direction 
+        if(this.pos.y < this.r && this.direction.y < 0)
             this.direction.y *= -1;
-        }*/ //delete once ball bounces off paddle
-        else if (this.pos.x < this.r && this.direction.x < 0) {
+        else if(this.pos.x < this.r && this.direction.x < 0)
             this.direction.x *= -1;
-        }
-        else if (this.pos.x > width - this.r && this.direction.x > 0) {
+        else if(this.pos.x > width - this.r && this.direction.x > 0)
             this.direction.x *= -1;
-        }
     }
-
-    this.meets = function(paddle) {
-        if (this.pos.y < paddle.pos.y && 
+    
+    this.meets = function(paddle){
+        if(this.pos.y < paddle.pos.y && 
             this.pos.y > paddle.pos.y - this.r &&
             this.pos.x > paddle.pos.x - this.r &&
-            this.pos.x < paddle.pos.x + paddle.w + this.r) {
+            this.pos.x < paddle.pos.x + paddle.w + this.r){
                 return true;
-        }
+        } 
         else {
             return false;
         }
+    }
+
+    this.hits = function(brick){
+        var distance = dist(this.pos.x, this.pos.y, brick.pos.x, brick.pos.y);
+        if(distance < this.r + brick.r) return true;
+        else return false;
     }
 }
